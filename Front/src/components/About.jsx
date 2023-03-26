2ce0955bb1999a677cdb5a5ed57472e50ee27f8a
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-03-25 21:08:13 -0300

    probando git

--- Front/src/components/About.jsx
+++ Front/src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (<div><img src="../../assets/bg3.png" alt="pinche imagen"/>
     <br/>
     <p>Mauro Moyano
-        Frontend Developer | HTML, CSS, JavaScript, ReactJS, Redux,
+        Frontend Developer | HTML, CSS, JavaScript, NextJs, ReactJS, Redux,
         Argentina
         Extracto
         Mis habilidades y tecnologías que manejo son: JavaScript ReactJs Redux Node Espress PostgreSQL Ingles: intermedio Habilidades blandas- Adaptabilidad- Proactividad- Resolución de problemas- Amabilidad y buenas relaciones interpersonales- Ganas de continuar aprendiendoMe encuentro abierto a encarar nuevos proyectos para poder desempeñarme en nuevos desafíos que me permitan aportar y enriquecer mis conocimientos.
